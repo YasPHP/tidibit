c7283df402578c23e6249d7868460754470ec47a
YasPHP <47681976+YasPHP@users.noreply.github.com>
2019-03-18 23:20:25 -0400

    Revisit __main__

---
+++ name.py
@@ -0,0 +1,36 @@
+# Top-Level Script Environment
+
+# Whenever the Python interpreter reads a source file, it does two things:
+
+# -it sets a few special variables like __name__, and then
+
+# -it executes all of the code found in the file.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------#
+
+
+class Vehicle:
+    def __init__(self, type, ):
+        if __name__ == '__main__':
+            username = "Joe"
+            age = 65
+            avatar1 = Avatar(username, age)
+            avatar1.greeting()
