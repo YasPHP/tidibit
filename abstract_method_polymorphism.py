02f6c0452779cd5e1e58a803fd50cd37cd0ce93e
YasPHP <47681976+YasPHP@users.noreply.github.com>
2019-04-04 10:09:44 -0400

    abstract method with polymorphic code

--- abstract_method_polymorphism.py
+++ abstract_method_polymorphism.py
@@ -1,13 +1,14 @@
+#super class
 class Animal:
     def __init__(self, name):    # Constructor of the class
         self.name = name
     def talk(self):              # Abstract method, defined by convention only
         raise NotImplementedError("Subclass must implement abstract method")
-
+#sub class
 class Cat(Animal):
     def talk(self):
         return 'Meow!'
-
+#sub class
 class Dog(Animal):
     def talk(self):
         return 'Woof! Woof!'
